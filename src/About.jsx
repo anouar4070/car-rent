8387af2b85bf8068987d43d850a3d79c852d6827
anouar garbaa <g.anouar@yahoo.com>
2024-08-26 11:43:47 +0100

    add About route

---
+++ src/About.jsx
@@ -0,0 +1,6 @@
+function About() {
+  return (
+    <h1>About page goes here! 🎉</h1>
+  )}
+
+  export default About
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import About from "./About.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+);
