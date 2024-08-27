73f3c4954c549a1e2ce5b35bb492027e383c9524
anouar garbaa <g.anouar@yahoo.com>
2024-08-27 01:52:26 +0100

    add index routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import './App.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import Layout from './components/Layout';
-import Vans from './pages/Vans/Vans';
-import VanDetail from './pages/Vans/VanDetail';
-import Dashboard from './pages/Host/Dashboard';
-import Income from './pages/Host/Income';
-import Reviews from './pages/Host/Reviews';
-import HostLayout from './components/HostLayout';
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./components/Layout";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import HostLayout from "./components/HostLayout";
 
-import "../server"
-
-
+import "../server";
 
 function App() {
   return (
     <BrowserRouter>
-     
-      <Routes>
-      <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-
-          <Route path="/host" element={<HostLayout />}>
-          <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+                    
+     <Routes>
+        <Route path="/" element={<Layout />}>   {/* <Route element={<Layout />}> */}
+          <Route index element={<Home />} />    {/* <Route path="/" element={<Home />} /> */}
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-
         </Route>
       </Routes>
     </BrowserRouter>
@@ -39,4 +36,3 @@ function App() {
 }
 
 export default App;
-
