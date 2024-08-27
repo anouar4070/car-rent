682d23601cbf3c3bfedaa181dd61f4c455346b9e
anouar garbaa <g.anouar@yahoo.com>
2024-08-27 01:39:19 +0100

    Creating the Host Layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,10 @@ import VanDetail from './pages/Vans/VanDetail';
 import Dashboard from './pages/Host/Dashboard';
 import Income from './pages/Host/Income';
 import Reviews from './pages/Host/Reviews';
+import HostLayout from './components/HostLayout';
 
 import "../server"
+
 
 
 function App() {
@@ -23,9 +25,13 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+
+          <Route path="/host" element={<HostLayout />}>
           <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
+
         </Route>
       </Routes>
     </BrowserRouter>
