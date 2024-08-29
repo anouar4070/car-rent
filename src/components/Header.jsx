48cfeceae63f51cb64cf233a1605baae2f408a19
anouar garbaa <g.anouar@yahoo.com>
2024-08-29 19:53:13 +0100

    Active Link Styling with NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -8,10 +8,25 @@ export default function Header() {
         #RentVan
       </Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-      </nav>
+                <NavLink 
+                    to="/host"
+                    className={({isActive}) => isActive ? "active-link" : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink 
+                    to="/about"
+                    className={({isActive}) => isActive ? "active-link" : null}
+                >
+                    About
+                </NavLink>
+                <NavLink 
+                    to="/vans"
+                    className={({isActive}) => isActive ? "active-link" : null}
+                >
+                    Vans
+                </NavLink>
+            </nav>
     </header>
   );
 }
