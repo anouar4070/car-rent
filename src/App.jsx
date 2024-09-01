17251074a6b8a616025599cee30817b71a7b3912
anouar garbaa <g.anouar@yahoo.com>
2024-09-01 15:17:55 +0100

    add auth when accessing to /Host

--- src/App.jsx
+++ src/App.jsx
@@ -17,55 +17,44 @@ import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
+import AuthRequired from "./components/AuthRequired";
 
 import "../server";
-
-
-
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+       <Route path="/" element={<Layout />}>
+
           {/* <Route element={<Layout />}> */}
           <Route index element={<Home />} />{" "}
           {/* <Route path="/" element={<Home />} /> */}
           <Route path="about" element={<About />} />
-
-          <Route path="vans" element={<Vans />} />
+           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
           <Route
             path="login"
             element={<Login />}
           />
 
-          {/* There is no sharing UI betwin vans & vanDetail so we don't put elemnt with outlet in the parent route */}
-          {/* <Route path="vans">
-            <Route index element={<Vans />} />
-            <Route path=":id" element={<VanDetail />} />
-          </Route> */}
-
-          <Route path="host" element={<HostLayout />}>
+       
+     <Route element={<AuthRequired />}>
+         <Route path="host" element={<HostLayout />}>
               <Route index element={<Dashboard />} />
               <Route path="income" element={<Income />} />
               <Route path="reviews" element={<Reviews />} />
               <Route path="vans" element={<HostVans />} />
-            
-{/*  a way to make link to=".." works as expected in HostVanDetail.jsx */}
-              {/* <Route path="vans" element={<Outlet />}>
-              <Route index element={<HostVans />} />
-              <Route path=":id" element={<HostVanDetail />} />
-                  </Route> */}
             <Route path="vans/:id" element={<HostVanDetail />}>
               <Route index element={<HostVanInfo />} />
               <Route path="pricing" element={<HostVanPricing />} />
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
-
-          </Route>
+         </Route>
+     </Route>
           <Route path="*" element={<NotFound />}/>
+
         </Route>
       </Routes>
     </BrowserRouter>
@@ -73,3 +62,17 @@ function App() {
 }
 
 export default App;
+
+
+   {/* There is no sharing UI betwin vans & vanDetail so we don't put elemnt with outlet in the parent route */}
+          {/* <Route path="vans">
+            <Route index element={<Vans />} />
+            <Route path=":id" element={<VanDetail />} />
+          </Route> */}
+
+
+{/*  a way to make link to=".." works as expected in HostVanDetail.jsx */}
+              {/* <Route path="vans" element={<Outlet />}>
+              <Route index element={<HostVans />} />
+              <Route path=":id" element={<HostVanDetail />} />
+                  </Route> */}
