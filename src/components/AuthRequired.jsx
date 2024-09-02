0ba5902ed6414ebd65cb8fe357da812b53d3f5a6
anouar garbaa <g.anouar@yahoo.com>
2024-09-02 23:08:03 +0100

    modify AuthRequired & Login.jsx to send user to original page

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -1,14 +1,20 @@
 import React from "react"
-import { Outlet, Navigate } from "react-router-dom"
+import { Outlet, Navigate, useLocation } from "react-router-dom"
 
 export default function AuthRequired() {
     const isLoggedIn = localStorage.getItem("loggedin")
+    const location = useLocation()
+    console.log(location)
+    //{pathname: "/host/vans", search: "", hash: "", state: null, key: "default"}
     
     if (!isLoggedIn) {
         return (
             <Navigate 
                 to="/login" 
-                state={{message: "You must log in first"}} 
+                state={{
+                    message: "You must log in first",
+                    from: location.pathname
+                }} 
                 replace
             />)
     }
